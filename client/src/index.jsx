61ec339f0c9d9f28678e2f881e9632584ec3f669
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-01 18:47:13 -0600

    Created boilerplate code for I Am Wintrow website.

---
+++ client/src/index.jsx
@@ -0,0 +1,27 @@
+import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import App from './components/App.jsx';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
